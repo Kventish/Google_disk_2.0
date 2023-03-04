1344c053faae9b56b6f3d8c1270d654fb42dd875
Kventish <dima30-02@mail.ru>
2023-03-04 17:23:49 +0600

    test

---
+++ config.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="check">
+        <p>Hello, World</p>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    .check{
+        margin-top: 10px;
+    }
+</style>
